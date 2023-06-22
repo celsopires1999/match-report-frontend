b24c3f81bf1d4d2ceac97caa5e2e5de62fc0c78d
celsopires1999 <celso.pires1999@gmail.com>
2023-06-22 17:45:29 +0000

    add material-ui

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+    "@mui/styles": {
+      transform: "@mui/styles/{{member}}",
+    },
+    "@mui/lab": {
+      transform: "@mui/lab/{{member}}",
+    },
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Login() {
+  return (
+    <div>
+      <Typography variant="h2" textAlign="center">
+        Hello, World!
+      </Typography>
+    </div>
+  );
+}
